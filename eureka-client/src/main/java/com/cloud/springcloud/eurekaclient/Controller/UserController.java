08af72c71f80355622b7839292401326322862ca
JiangCloud <cloud289640936@hotmail.com>
2019-09-05 15:48:37 +0800

    springcloud 示例

---
+++ eureka-client/src/main/java/com/cloud/springcloud/eurekaclient/Controller/UserController.java
@@ -0,0 +1,21 @@
+package com.cloud.springcloud.eurekaclient.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/pushInfos")
+    public String pushInfo(){
+
+        return "I am  chinese";
+    }
+
+    public  String  signGirl(){
+
+        return "";
+    }
+}
